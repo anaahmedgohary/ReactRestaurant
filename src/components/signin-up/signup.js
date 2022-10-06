c176f880802630ebdd47ccf795fd38e880a33e2b
anaahmedgohary <gogoahmed13@gmail.com>
2022-10-06 06:00:24 +0200

    29

--- src/components/signin-up/signup.js
+++ src/components/signin-up/signup.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./style/signin.css";
 import { useState } from "react";
+import $ from "jquery";
 //import axios from 'axios';
 import { createClient } from '@supabase/supabase-js';
 
@@ -25,67 +26,38 @@ export default function SignUp()
     setIpassword(document.getElementById("password").value);
     
   }
-
   
+  // console.log(iemail);
+  // console.log(ipassword);
   
-  console.log(iemail);
-  console.log(ipassword);
-  
-
-  //const supabaseUrl = 'https://rfjecvakijoqooiktpon.supabase.co';
-  //const supabaseKey = process.env.SUPABASE_KEY;
-  //const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmamVjdmFraWpvcW9vaWt0cG9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ4NTM2MDIsImV4cCI6MTk4MDQyOTYwMn0.Ryea4rE_iOPb49WzUhvHZiOaKMkL_48kNPMVejaeP_M';
-  //const supabase = createClient(supabaseUrl, supabaseKey);
-  //console.log(supabase);
-
   const handleSubmit = async (event) =>
   {
     
     event.preventDefault();
-    setIemail(iemail);
-    setIpassword(ipassword);
-
     //axios.defaults.baseURL = process.env.APP_URL
     // process.env.NOW_URL
     const supabaseUrl = 'https://rfjecvakijoqooiktpon.supabase.co';
     //const supabaseKey = process.env.SUPABASE_KEY;
     const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmamVjdmFraWpvcW9vaWt0cG9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ4NTM2MDIsImV4cCI6MTk4MDQyOTYwMn0.Ryea4rE_iOPb49WzUhvHZiOaKMkL_48kNPMVejaeP_M';
     const supabase = createClient(supabaseUrl, SUPABASE_ANON_KEY);
-    //const { inputs: newuser, error } = await supabase.from('newuser').select('*')
     const { data, error } = await supabase
       .from('newuser')
       .insert([
         { email: `${iemail}`, password: `${ipassword}` }
         
-      ]);
+    ]);
+    
+    $("#submitSignUp").off("click");
+    $("#submitSignUp").prop('disabled', true);
+    $("#submitSignUp").hide();
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+    alert("Thank You!")
     console.log(error);
     console.log(data);
-
-    //let baseURL = "https://rfjecvakijoqooiktpon.supabase.co/api/";
-    // https://app.supabase.com/project/rfjecvakijoqooiktpon/storage/buckets/api
-
-    // https://react-restaurant-h47a0xx0f-ahmedgohary.vercel.app      //process.env.API_URL;  //process.env.NOW_URL
-    // http://localhost:80/api/
-
-    //axios.post(supabase, inputs
-      // {
-        // headers: {
-        //   //'Content-Type': 'https://react-restaurant-php.vercel.app',
-        //   'Content-type': 'application/x-www-form-urlencoded',
-        //   'Access-Control-Allow-Origin': "*",
-        //   //'Access-Control-Allow-Headers': '*',
-        //   'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-        // }
-    //);
-    
-    
-
-
     
   }
     
-
-
 
     return (
       <div className="container">
@@ -93,8 +65,6 @@ export default function SignUp()
           <h6>Sign Up</h6>
           <div>
             <form
-              // action="./signup.php"
-              // method="post"
               id="signinForm"
               onSubmit={handleSubmit}
             >
@@ -113,7 +83,7 @@ export default function SignUp()
                 Password
               </label>
               <input
-                type="text"
+                type="password"
                 name="password"
                 id="password"
                 required
@@ -124,7 +94,8 @@ export default function SignUp()
                 <input
                   className="submit-btn btn btn-success"
                   type="submit"
-                  value="Log In"
+                  value="Sign Up"
+                  id='submitSignUp'
                 />
               </div>
             </form>
