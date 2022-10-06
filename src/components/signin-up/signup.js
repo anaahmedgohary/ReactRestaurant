2f5cae94e9d40774bc7a3080b7f9c0b20dd68529
anaahmedgohary <gogoahmed13@gmail.com>
2022-10-06 04:25:16 +0200

    27 oh wow

--- src/components/signin-up/signup.js
+++ src/components/signin-up/signup.js
@@ -6,6 +6,7 @@ import { createClient } from '@supabase/supabase-js';
 
 export default function SignUp()
 {
+  
   const [inputs, setInputs] = useState({})
 
   const handleChange = (event) =>
@@ -22,16 +23,23 @@ export default function SignUp()
   //const supabase = createClient(supabaseUrl, supabaseKey);
   //console.log(supabase);
 
-  const handleSubmit = (event) =>
+  const handleSubmit = async (event) =>
   {
+    
     event.preventDefault();
 
     //axios.defaults.baseURL = process.env.APP_URL
     // process.env.NOW_URL
     const supabaseUrl = 'https://rfjecvakijoqooiktpon.supabase.co';
     //const supabaseKey = process.env.SUPABASE_KEY;
-    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmamVjdmFraWpvcW9vaWt0cG9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ4NTM2MDIsImV4cCI6MTk4MDQyOTYwMn0.Ryea4rE_iOPb49WzUhvHZiOaKMkL_48kNPMVejaeP_M';
-    const supabase = createClient(supabaseUrl, supabaseKey);
+    const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmamVjdmFraWpvcW9vaWt0cG9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ4NTM2MDIsImV4cCI6MTk4MDQyOTYwMn0.Ryea4rE_iOPb49WzUhvHZiOaKMkL_48kNPMVejaeP_M';
+    const supabase = createClient(supabaseUrl, SUPABASE_ANON_KEY);
+    //const { inputs: newuser, error } = await supabase.from('newuser').select('*')
+    const { inputs, error } = await supabase
+      .from('newuser')
+      .insert([
+        { email: 'The Shire', password: 554 }
+      ]);
     //console.log(supabase);
 
     //let baseURL = "https://rfjecvakijoqooiktpon.supabase.co/api/";
@@ -40,7 +48,7 @@ export default function SignUp()
     // https://react-restaurant-h47a0xx0f-ahmedgohary.vercel.app      //process.env.API_URL;  //process.env.NOW_URL
     // http://localhost:80/api/
 
-    axios.post(supabase, inputs
+    //axios.post(supabase, inputs
       // {
         // headers: {
         //   //'Content-Type': 'https://react-restaurant-php.vercel.app',
@@ -49,7 +57,11 @@ export default function SignUp()
         //   //'Access-Control-Allow-Headers': '*',
         //   'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
         // }
-      );
+    //);
+    
+    
+
+
     console.log(inputs);
   }
     
