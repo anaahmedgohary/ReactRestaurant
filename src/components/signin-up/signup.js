1f5c0477d0396c0afcdb2afe29bce0fc52922fa4
anaahmedgohary <gogoahmed13@gmail.com>
2022-10-06 03:09:43 +0200

    db22

--- src/components/signin-up/signup.js
+++ src/components/signin-up/signup.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./style/signin.css";
 import { useState } from "react";
 import axios from 'axios';
+import { createClient } from '@supabase/supabase-js';
 
 export default function SignUp()
 {
@@ -14,16 +15,30 @@ export default function SignUp()
     setInputs(values => ({ ...values, [name]: value }));
     
   }
-  const handleSubmit = (event) => {
+
+  const supabaseUrl = 'https://rfjecvakijoqooiktpon.supabase.co';
+    const supabaseKey = process.env.SUPABASE_KEY;
+    const supabase = createClient(supabaseUrl, supabaseKey);
+    console.log(supabase);
+
+  const handleSubmit = (event) =>
+  {
     event.preventDefault();
 
     //axios.defaults.baseURL = process.env.APP_URL
     // process.env.NOW_URL
-    let baseURL = "https://react-restaurant-h47a0xx0f-ahmedgohary.vercel.app/";
+    const supabaseUrl = 'https://rfjecvakijoqooiktpon.supabase.co';
+    const supabaseKey = process.env.SUPABASE_KEY;
+    const supabase = createClient(supabaseUrl, supabaseKey);
+    console.log(supabase);
+
+    //let baseURL = "https://rfjecvakijoqooiktpon.supabase.co/api/";
+    // https://app.supabase.com/project/rfjecvakijoqooiktpon/storage/buckets/api
+
     // https://react-restaurant-h47a0xx0f-ahmedgohary.vercel.app      //process.env.API_URL;  //process.env.NOW_URL
     // http://localhost:80/api/
 
-    axios.post(baseURL, inputs,
+    axios.post(`${supabase}`, inputs,
       {
         headers: {
           //'Content-Type': 'https://react-restaurant-php.vercel.app',
@@ -34,70 +49,63 @@ export default function SignUp()
         }
       });
     console.log(inputs);
-
-    // fetch("mAgfFs4y1Dt1ERMjSZCNhprH", {
-    //   // Enter your IP address here
-    // axios.defaults.baseURL = process.env.APP_URL
-    //   method: "POST",
-    //   // url: "./api/index.php",
-    //   mode: "cors",
-    //   body: JSON.stringify(inputs),
-    // });
-  };
+  }
+    
 
 
 
-  return (
-    <div className="container">
-      <div id="signupDiv">
-        <h6>Sign Up</h6>
-        <div>
-          <form
-            // action="./signup.php"
-            // method="post"
-            id="signinForm"
-            onSubmit={handleSubmit}
-          >
-            <label className="label-signin" htmlFor="email">
-              E-mail
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              required
-              onChange={handleChange}
-            />
-            <label className="label-signin" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="text"
-              name="password"
-              id="password"
-              required
-              onChange={handleChange}
-            />
-            <div>
+    return (
+      <div className="container">
+        <div id="signupDiv">
+          <h6>Sign Up</h6>
+          <div>
+            <form
+              // action="./signup.php"
+              // method="post"
+              id="signinForm"
+              onSubmit={handleSubmit}
+            >
+              <label className="label-signin" htmlFor="email">
+                E-mail
+              </label>
               <input
-                className="submit-btn btn btn-success"
-                type="submit"
-                value="Log In"
+                type="email"
+                name="email"
+                id="email"
+                required
+                onChange={handleChange}
               />
-            </div>
-          </form>
-        </div>
-        <div>
-          <a
-            className="forgot-pass"
-            href="./forgotpass"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            forgot password ?
-          </a>
+              <label className="label-signin" htmlFor="password">
+                Password
+              </label>
+              <input
+                type="text"
+                name="password"
+                id="password"
+                required
+                onChange={handleChange}
+              />
+              <div>
+                <input
+                  className="submit-btn btn btn-success"
+                  type="submit"
+                  value="Log In"
+                />
+              </div>
+            </form>
+          </div>
+          <div>
+            <a
+              className="forgot-pass"
+              href="./forgotpass"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              forgot password ?
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+
